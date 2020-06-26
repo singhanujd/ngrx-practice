fb5d79795d9b9d6dfa582c0813421a4e93e9d7de
singhanujd <singhanujd@gmail.com>
2020-06-26 12:39:28 +0530

    Ngrx Basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import {StoreModule} from '@ngrx/store';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import {counterReducer} from './counter.reducer';
+import { MyCounterComponent } from './my-counter/my-counter.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [
+   BrowserModule,
+   FormsModule,
+   StoreModule.forRoot({count: counterReducer}) 
+  ],
+  declarations: [ AppComponent, HelloComponent, MyCounterComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
